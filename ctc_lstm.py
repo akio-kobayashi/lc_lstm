019562e12986fa30f9c34d0fb5f6e7ac6a4f6718
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2020-01-16 10:46:42 +0900

    minor revision

--- ctc_lstm.py
+++ ctc_lstm.py
@@ -149,7 +149,8 @@ def main():
             # the function returns ['loss', 'ler', 'ser']
             # 'ler' should not be normalized by true lengths
             loss, ler, ser = model.test_on_batch(x=data)
-            print("%.4f" % (loss))
+            for k in range(len(loss))
+                print("%.4f" % loss[k])
             # for micro-mean
             samples = data[0].shape[0]
             curr_val_loss += np.sum(np.array(loss)) * samples
