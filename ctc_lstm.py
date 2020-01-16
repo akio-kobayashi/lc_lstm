6a4626d46cc942fa794656b4f41ba3b6231aead0
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2020-01-16 17:37:57 +0900

    minor revision

--- ctc_lstm.py
+++ ctc_lstm.py
@@ -12,6 +12,7 @@ import keras.backend as K
 import numpy as np
 import random
 import tensorflow as tf
+import tf.keras
 #from tf.keras.experimental import PeeholeLSTMCell
 #import functools
 import CTCModel
