04a87cd672c406d57d584d6a9bf4f59a676178de
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2020-01-16 20:58:48 +0900

    minor revision

--- ctc_lstm.py
+++ ctc_lstm.py
@@ -134,9 +134,9 @@ def main():
             loss, ler, _ = model.evaluate(data)
             curr_ler += np.sum(np.array(ler))
 
-            pred = model.predict([data[0], data[3]], batch_size=args.batch_size, max_value=args.n_labels)
-            for i in range(10):  # print the 10 first predictions
-                print("Prediction :", [j for j in pred[i]] )
+            #pred = model.predict([data[0], data[3]], batch_size=args.batch_size, max_value=args.n_labels)
+            #for i in range(10):  # print the 10 first predictions
+                #print("Prediction :", [j for j in pred[i]] )
                 #print("Prediction :", [j for j in pred[i] if j!=-1] )
             # progress report
             progress_loss = curr_loss/curr_samples
