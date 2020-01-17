ce292c19016b0281591cdf962547d039e6c6aed0
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2020-01-17 09:35:36 +0900

    minor revision

--- train_ctc.sh
+++ train_ctc.sh
@@ -22,7 +22,7 @@ factor=0.9
 
 mkdir -p ./snaps
 mkdir -p ./logs
-python ctc_lstm.py --data $train --key-file $keys --valid $valid \
+python ctc_lstm.py --data $train --valid $valid \
   --feat-dim $feat_dim --n-labels $n_labels --batch-size $batch_size --epochs $epochs \
   --snapshot ./snaps  --learn-rate $learn_rate --log-dir ./logs \
   --units $units --lstm-depth $lstm_depth --factor $factor
