2ac39ff64977c299e98e6ec6304206d86d5b7fb2
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2020-01-18 10:48:13 +0900

    minor revision

--- ctc_lstm.py
+++ ctc_lstm.py
@@ -144,7 +144,7 @@ def main():
                 progress_ler = np.mean(curr_ler)*100.0
                 msg='progress: (%d/%d) loss=%.4f ler=%.4f' % (bt+1,training_generator.__len__(), progress_loss, progress_ler)
                 print(msg,file=sys.stderr)
-                logs.writeline(msg)
+                logs.write(msg+'\n')
             print('\n',end='',file=sys.stderr)
             curr_loss /= curr_samples
             curr_ler = np.mean(curr_ler)*100.0
@@ -166,7 +166,7 @@ def main():
                 curr_val_ler.append(ler)
 
             msg='Epoch %d (train) loss=%.4f ler=%.4f' % (ep+1, curr_loss, curr_ler)
-            logs.writeline(msg)
+            logs.write(msg+'\n')
             print(msg,file=sys.stderr)
 
             curr_val_loss /= curr_val_samples
@@ -180,7 +180,7 @@ def main():
                         curr_lr = args.min_lr
                     else:
                         msg="lerning rate chaged %.4f to %.4f" % (prev_lr, curr_lr)
-                        logs.writeline(msg)
+                        logs.write(msg+'\n')
                         print(msg, file=sys.stderr)
                         K.set_value(model.model_train.optimizer.lr,curr_lr)
                     patience=0
@@ -189,7 +189,7 @@ def main():
 
             #print('Epoch %d (valid) ler=%.4f' % (ep+1, curr_val_ler), file=sys.stderr)
             msg='Epoch %d (valid) loss=%.4f ler=%.4f' % (ep+1, curr_val_loss, curr_val_ler)
-            logs.writeline(msg)
+            logs.write(msg+'\n')
             print(msg, file=sys.stderr)
 
             # save best model in .h5
