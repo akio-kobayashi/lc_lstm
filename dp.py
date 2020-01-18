77858b42b2a91d9a4ae80bb6b211668a0192a631
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2020-01-18 23:19:15 +0900

    minor revision

--- dp.py
+++ dp.py
@@ -1,26 +1,25 @@
 import numpy as np
 
-def dynamic_programming(scores, labels, n_labels):
+def dynamic_programming(scores, labels, n_inputs, n_labels):
     '''
     params:
         scores: 2-d np.array with shape=(frames, n_labels), log-scores
         labels: 1-d np.array w/o blanks
-        return: 1-d np.array
+    return: 1-d np.array
     '''
-    frames = scores.shape[0]
     seqlen = 2*labels.shape[0]+1
 
     labels_blanks = np.full(n_labels)
     for n in range(labels.shape[0]):
         labels_blanks[2*n+1] = labels[n]
 
-    dpmat = np.full((frames, seqlen), -1.0e10)
-    bptr  = np.full((frames, seqlen), -1)
+    dpmat = np.full((n_inputs, seqlen), -1.0e10)
+    bptr  = np.full((n_inputs, seqlen), -1)
     # init
     dpmat[0][0] = scores[0][n_labels]
     dpmat[0][1] = scores[0][labels_blanks[1]]
 
-    for f in range(frames):
+    for f in range(n_inputs):
         if f == 0 continue
         for curr_state in (seqlen):
             if labels_blanks[curr_state]==n_labels:
@@ -38,12 +37,12 @@ def dynamic_programming(scores, labels, n_labels):
             dpmat[f][curr_state] = min_score
             bptr[f][curr_state] = p
     final_state = seqlen-1
-    if dpmat[frames-1][final_state-1] > dpmat[frames-1][final_state-2]:
+    if dpmat[n_inputs-1][final_state-1] > dpmat[n_inputs-1][final_state-2]:
         final_state = final_state-1
 
     results=[]
     state = final_state
-    fpt=frames-1
+    fpt=n_inputs-1
     while state >= 0:
         results.append(state)
         state=bptr[fpt][state]
@@ -51,4 +50,3 @@ def dynamic_programming(scores, labels, n_labels):
     results = results[::-1]
 
     return results
-                
