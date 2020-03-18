4a8a9c0abb78d86ee754eac27e0f7a22ebc5b8e1
akio-kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2020-03-18 11:35:54 +0900

    minor revision

--- td_eval_multi_lc.sh
+++ td_eval_multi_lc.sh
@@ -3,10 +3,10 @@
 export CUDA_VISIBLE_DEVICES=0
 
 # librispeech
-valid=./dev_clean.h5
-test=./test_clean.h5
-n_labels=32
-prior=./ls_label_counts.h5
+valid=./dev.h5
+test=./test.h5
+n_labels=49
+prior=./td_label_counts.h5
 
 # features
 feat_dim=40
@@ -20,16 +20,16 @@ dropout=0.0
 optim=adadelta
 filters=32
 
-proc_frames=20
-extra_frames1=20
-extra_frames2=20
+proc_frames=10
+extra_frames1=10
+extra_frames2=10
 num_procs=1
 
-snapdir=./ls/lc_model_d${lstm_depth}_d${units}_f${filters}_l${learn_rate}
+#td/lc_model_d4_d256_f32_l1.0_B256_D0.0_f0.9_LNtrue_vgg_lstm_p10_e110_e210_adadelta_bi/
+snapdir=./td/lc_model_d${lstm_depth}_d${units}_f${filters}_l${learn_rate}
 snapdir=${snapdir}_B${batch_size}_D${dropout}
 snapdir=${snapdir}_f${factor}_LNtrue_vgg_lstm
-snapdir=${snapdir}_p${proc_frames}_ef${extra_frames1}_es${extra_frames2}
-snapdir=${snapdir}_n${num_procs}
+snapdir=${snapdir}_p${proc_frames}_e1${extra_frames1}_e2${extra_frames2}
 snapdir=${snapdir}_${optim}_${direction}
 
 weights=${snapdir}/snapshot.h5
